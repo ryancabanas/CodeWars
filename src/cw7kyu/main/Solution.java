adfe7c295035dbbc0320a77c5a6dc1ba3fc50669
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-06-15 11:37:34 -0400

    Stuff

--- src/cw7kyu/main/Solution.java
+++ src/cw7kyu/main/Solution.java
@@ -9,5 +9,30 @@ public class Solution {
   
   public static int numberOfRectangles(int m, int n) {
     return ((m * (m + 1)) / 2) * ((n * (n + 1)) / 2);
-  }  
+  }
+  
+  public static String isSortedAndHow(int[] array) {
+    boolean asc = true;
+    boolean desc = true;
+    
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] > array[i-1]) {
+        desc = false;
+      }
+      if (array[i] < array[i-1]) {
+        asc = false;
+      }
+      if (!desc & !asc) {
+        break;
+      }
+    }
+    
+    return (!asc & !desc) ? "no" : (asc) ? "yes, ascending" : "yes, descending";
+  }
+}
+
+class TestIt {
+  public static void main(String[] args) {
+    System.out.println(Solution.isSortedAndHow(new int[] {15, 7, 3, -8}));
+  }
 }
