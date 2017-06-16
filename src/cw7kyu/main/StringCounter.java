bf9d0acc027245bb48ea9b4fea8d3170f5e21026
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-06-16 16:35:42 -0400

    kata work

---
+++ src/cw7kyu/main/StringCounter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cw7kyu.main;
+
+/**
+ *
+ * @author Ryan Cabanas
+ */
+public class StringCounter {
+  
+}
